0e0988a551a7782e42f996cf6b01671289a1ea22
suite <gr8bitmail@gmail.com>
2024-11-09 18:45:37 -0700

    started deck and card, starting action

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,134 @@
-use std::{error::Error, fmt::Display, io};
+use std::{error::Error, fmt::{write, Display}, io};
+
+struct Deck {
+    cards: Vec<Card>
+}
+
+impl Deck {
+    fn take_card(&mut self) -> Card {
+        match self.cards.pop() {
+            Some(card) => card,
+            None => todo!("Handle out of cards")
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Suite {
+    Heart,
+    Diamond,
+    Club,
+    Spade
+}
+
+impl Suite {
+    fn iterator() -> impl Iterator<Item = Self> {
+        [Suite::Heart, Suite::Diamond, Suite::Club, Suite::Spade].iter().copied()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Rank {
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace
+}
+
+impl Rank {
+    fn iterator() -> impl Iterator<Item = Self> {
+        [Rank::Two, Rank::Three, Rank::Four, Rank::Five, Rank::Six, Rank::Seven, Rank::Eight, Rank::Nine, Rank::Ten, Rank::Jack, Rank::Queen, Rank::King, Rank::Ace].iter().copied()
+    }
+
+    fn value(&self) -> (u8, Option<u8>) {
+        match &self {
+            Rank::Two => (2, None),
+            Rank::Three => (3, None),
+            Rank::Four => (4, None),
+            Rank::Five => (5, None),
+            Rank::Six => (6, None),
+            Rank::Seven => (7, None),
+            Rank::Eight => (8, None),
+            Rank::Nine => (9, None),
+            Rank::Ten => (10, None),
+            Rank::Jack => (10, None),
+            Rank::Queen => (10, None),
+            Rank::King => (10, None),
+            Rank::Ace => (1, Some(11)),
+        }
+    } 
+}
+
+#[derive(Debug)]
+struct Card {
+    rank: Rank,
+    suite: Suite,
+}
 
 pub struct BlackJack {
     player: Player,
     turn: Turn,
     bet_amount: u32,
+    deck: Deck,
+
+    current_hand_index: usize, // TODO: move to ref?
+    player_hands: Vec<Hand>, 
+    dealer_hand: Hand
+}
+
+
+#[derive(Debug)]
+struct Hand {
+    cards: Vec<Card>
+}
+
+// impl Display for Hand {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         // [Card { rank: Ace, suite: Diamond }, Card { rank: Three, suite: Spade }]
+//         // A<> 3S
+//         todo!("do sum");
+//         let mut card_str = String::new();
+//         for card in &self.cards {
+//             write!(f, "{}", card_str)
+//         }
+//         Ok(())
+        
+//     }
+// }
+
+impl Hand {
+    fn get_value(&self) -> (u8, Option<u8>) {
+        let mut value = 0;
+        let mut option_val: Option<(u8 ,u8)> = None;
+
+        for card in &self.cards {
+            let (num1, num2) = card.rank.value();
+            match num2 {
+                Some(num2) => {
+                    match option_val {
+                        Some((prev1, prev2)) => option_val = Some((prev1+num1, prev2+num2)),
+                        None => option_val = Some((num1, num2))
+                    }
+                },
+                None => value += num1
+            }
+        }
+
+        if let Some((num1, num2)) = option_val {
+            return (value+num1, Some(value+num2));  
+        }
+
+        (value, None)
+    }
 }
 
 pub struct Player {
@@ -41,7 +166,7 @@ impl Player {
             .read_line(&mut balance)
             .expect("Failed to read balance");
 
-        let balance = match balance.trim().parse() {
+        let balance: u32 = match balance.trim().parse() {
             Ok(num) => num,
             Err(_) => return Err("Failed to parse balance")
         };
@@ -88,34 +213,110 @@ impl Player {
     }
 }
 
+mod card_utils {
+    use rand::{seq::SliceRandom, thread_rng};
+    use super::{Card, Suite, Rank, Deck};
+    
+    fn generate_deck() -> Deck {
+        let mut cards = Vec::new();
+        
+        for rank in Rank::iterator() {
+            for suite in Suite::iterator() {
+                cards.push(Card {
+                    rank,
+                    suite
+                });
+            }
+        }
+
+        Deck {
+            cards
+        }
+    }
+
+    pub fn generate_blackjack_deck(num_decks: u32) -> Deck {
+        let mut cards = Vec::new();
+
+        for _ in 0..num_decks {
+            cards.append(&mut generate_deck().cards);
+        }
+
+        cards.shuffle(&mut thread_rng()); 
+
+        Deck {
+            cards
+        }
+    }
+}
+
 impl BlackJack {
     pub fn new(player: Player) -> Result<Self, &'static str> {
         let bet_amount = player.request_bet_amount().expect("bad bet amount");
+        let mut deck = card_utils::generate_blackjack_deck(6);
+
+        // player starts with two cards
+        // dealer starts with 1 card
+        let player_hands = vec![ Hand { cards: vec![deck.take_card(), deck.take_card()] }];
+        let dealer_hand = Hand { cards: vec![deck.take_card()] };
+
+        println!("Dealer shows {:?}", dealer_hand);
+        println!("Player shows {:?}", player_hands);
 
         Ok(BlackJack {
             player,
             turn: Turn::Player,
-            bet_amount
+            bet_amount,
+            deck,
+            current_hand_index: 0,
+            player_hands,
+            dealer_hand
         })
     }
 
-    pub fn get_player_action(&self) ->  Result<Action, &'static str>  {
-        let action = self.player.request_action().expect("bad action");
-        Ok(action)
+    pub fn take_action(&mut self) {
+        match self.turn {
+            Turn::Player => {
+                let action = self.player.request_action().expect("bad action");
+                self.take_player_action(action); 
+            },
+            Turn::Dealer => self.take_dealer_action(),
+        }
     }
 
-    pub fn deal_cards(&self) {
-        println!("Dealing cards...");
-    }
-
-    pub fn take_player_action(&mut self, action: Action) {
+    fn take_player_action(&mut self, action: Action) {
         println!("Taking action {action:?}");
+        match action {
+            Action::Hit => {
+                // add card to current hand 
+                // self.get_current_hand() ?
+                let curr_hand = self.player_hands.get_mut(self.current_hand_index).unwrap();
+                let curr_hand_cards = &mut curr_hand.cards;
+
+                curr_hand_cards.push(self.deck.take_card());
+
+                // print hand
+                println!("{curr_hand_cards:?}");
+                println!("value: {:?}", curr_hand.get_value());
+
+                // bust or 21, move to next hand
+            },
+            Action::Split => {
+                // split cards (check balance)
+            },
+            Action::Double => {
+                // double bet (check bet_amount and balance) => stand
+                // not available if split (not always true)
+            },
+            Action::Stand => {
+                // Move to next hand if split, otherwise dealer show 
+            },
+        }
 
         // depending on what we do, we want to change turn
-        self.turn = Turn::Dealer
+        // self.turn = Turn::Dealer
     }
 
-    pub fn take_dealer_action(&mut self) {
+    fn take_dealer_action(&mut self) {
         println!("Taking dealer action...");
 
         // depending on what we do, we want to change turn
@@ -124,9 +325,5 @@ impl BlackJack {
 
     pub fn is_running(&self) -> bool{
         return true;
-    }
-
-    pub fn get_turn(&self) -> &Turn {
-        &self.turn
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
 
-use std::{io::Split, process};
+use std::process;
 
-use blackjack::{BlackJack,Player,Turn};
+use blackjack::{BlackJack,Player};
+
+/*
+TODO:
+add some form of rust lint
+*/
 
 fn main() {
-    // enter name, enter balance
     let player = Player::new().unwrap_or_else(|err| {
         println!("Could not get player: {err}");
         process::exit(1);
     });
 
-    // do we want this to be mutable?
     let mut game = BlackJack::new(player).expect("couldnt create game");
-
-    game.deal_cards();
 
     // new round
     while game.is_running() { 
         // action start at player
-        let turn = game.get_turn(); // enum, Player, Dealer
-
-        match turn {
-            Turn::Player => {
-                let action = game.get_player_action().expect("should have player action"); 
-                game.take_player_action(action); // could be game.take_action()
-            },
-            Turn::Dealer => game.take_dealer_action() // show card or hit
-        }
+        game.take_action();
 
         // game.take_player_action() // hit, split, double, stand
         // if double/stand -> dealer show card
